08552e45254784bbb7165282bc7460f64cd2f3c9
joshgallantt <test@icloud.com>
2025-07-01 18:29:22 +0100

    init project

---
+++ example/nexus-logger-example/nexus-logger-example/nexus_logger_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexus_logger_exampleApp.swift
+//  nexus-logger-example
+//
+//  Created by Josh Gallant on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct nexus_logger_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
